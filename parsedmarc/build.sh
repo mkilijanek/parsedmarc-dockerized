b83a76c28e100f90d5de4ee74e96bd90839be106
mkilijanek <mkilijanek@users.noreply.github.com>
2026-03-10 17:29:32 +0100

    Release parsedmarc 9.2.0

---
+++ parsedmarc/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PARSEDMARC_VERSION=${PARSEDMARC_VERSION:-9.2.0}
+IMAGE_TAG=${IMAGE_TAG:-$PARSEDMARC_VERSION}
+IMAGE_NAME=${IMAGE_NAME:-ghcr.io/mkilijanek/parsedmarc}
+
+docker build \
+  --build-arg PARSEDMARC_VERSION=${PARSEDMARC_VERSION} \
+  --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+  --build-arg BUILD_DATE=$(date -u +%Y-%m-%dT%H:%M:%SZ) \
+  -t ${IMAGE_NAME}:${IMAGE_TAG} .
+
+if [ -n "$GHCR_TOKEN" ]; then
+  echo "$GHCR_TOKEN" | docker login ghcr.io -u "${GITHUB_USER}" --password-stdin
+  docker push ${IMAGE_NAME}:${IMAGE_TAG}
+fi
